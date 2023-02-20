fd5b82079614ed2a9125676f47adde0a63bbfeea
MatthewDevv <117437977+MatthewWDFD@users.noreply.github.com>
2023-02-20 18:24:58 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+  const forms = document.querySelectorAll(".needs-validation");
+  const name = document.getElementsByName("name")[0];
+  const surname = document.getElementsByName("surname")[0];
+  const tel = document.getElementsByName("tel")[0];
+  const email = document.getElementsByName("email")[0];
+  const message = document.getElementsByName("message")[0];
+  const emailed = document.querySelector(".emailed");
+  let formData = {};
+
+  Array.from(forms).forEach((form) => {
+    form.addEventListener("submit", (event) => {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      } else {
+        event.preventDefault();
+        formData.name = name.value;
+        formData.surname = surname.value;
+        formData.tel = tel.value;
+        formData.email = email.value;
+        formData.message = message.value;
+        emailed.classList.remove("d-none");
+        emailed.classList.add("d-block");
+      }
+
+      form.classList.add("was-validated");
+    });
+  });
+};
